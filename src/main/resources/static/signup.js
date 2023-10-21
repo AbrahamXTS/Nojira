28a7862e92f5215a27891d3ca8ab9268469820b1
Abraham Espinosa <abrahamespinosa3132@gmail.com>
2023-10-21 15:04:19 -0600

    fix: :bug: JS resources redirects to login page

--- src/main/resources/static/signup.js
+++ src/main/resources/static/signup.js
@@ -1,12 +1,13 @@
-console.log("Botón de cierre hace clic");
-const closeButtons = document.querySelectorAll(".close-btn");
-closeButtons.forEach((closeButton) => {
+// TODO: Hacer que los cleanInputButtons unicamente se muestren si hay algo en el input
+
+const cleanInputButtons = document.querySelectorAll(".close-btn");
+
+cleanInputButtons.forEach((closeButton) => {
     closeButton.addEventListener("click", () => {
         const input = closeButton.parentElement.querySelector("input");
+
         if (input) {
             input.value = "";
         }
     });
 });
-
-
