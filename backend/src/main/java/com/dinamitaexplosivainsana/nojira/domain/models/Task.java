2f65588f4314ca5598a61d94de1d07cdb6d68d77
Cristian Pan <a16003566@alumnos.uady.mx>
2023-11-18 11:45:18 -0600

    feat: :sparkles: Add and update models
    Added the following models: Role, Role Catalog, RoleCatalogEnum, Status, StatusCatalogEnum. And it was modifying the Task model.

--- backend/src/main/java/com/dinamitaexplosivainsana/nojira/domain/models/Task.java
+++ backend/src/main/java/com/dinamitaexplosivainsana/nojira/domain/models/Task.java
@@ -1,6 +1,3 @@
 package com.dinamitaexplosivainsana.nojira.domain.models;
-
-import java.util.Date;
-
-public record Task(String id, String description, Integer timeEstimatedInMinutes, String title, Integer timeUsedInMinutes) {
+public record Task(String id, String description, Integer timeEstimatedInMinutes, String title, Integer timeUsedInMinutes, User userAsigned, Project projectBelonging, Status status) {
 }
